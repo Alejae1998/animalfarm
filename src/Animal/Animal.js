28f56968f6d6537c773c2304a41fa6da0ba50607
Alejae1998 <alejagr1998@gmail.com>
2022-06-07 18:03:52 -0700

    trying to fix images

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -4,9 +4,9 @@ import './Animal.css';
 export default function Animal({ name, type, says }) {
   return (
     <div className="animal">
-      <h2 className="name">{name}</h2>
-      <span>{says}</span>
-      <img src={`/public/animals${type}.svg`}/>
+      <p className="name">{name}</p>
+      <p>{says}</p>
+      <img src={`/public/images${type}.svg`}/>
     </div>
   );
 }
